ba020278ca57463ebbad719cf21122d3f1596ac8
oubi-aed <alexander.oberhoer23@gmail.com>
2025-06-05 19:13:06 +0200

    can send control sum

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stm32f0xx.h>
 #include <string.h>
+#include <stdbool.h>
 #include "clock_.h"
 #include "fifo.h"
 #define LOG( msg... ) printf( msg );
@@ -9,6 +10,9 @@
 #define BAUDRATE 115200 // Baud rate set to 115200 baud per second
 
 #define MESSAGE_SIZE 64 // Size of the message buffer
+#define COL_FIELD 10 // Number of columns in the battlefield
+#define ROWS_FIELD 10 // Number of rows in the battlefield
+#define active_battlefield first_battlefield // Pointer to the active battlefield array
 
 const char USER_NAME[] = "Alexander"; // Name of the user, can be used for identification
 
@@ -17,8 +21,8 @@ volatile Fifo_t usart_rx_fifo;
 const uint8_t USART2_RX_PIN = 3; // PA3 is used as USART2_RX
 const uint8_t USART2_TX_PIN = 2; // PA2 is used as USART2_TX
 
-int first_battlefield[10*10] = {
-  2, 2, 0, 3, 3, 3, 0, 0, 0, 0, //Row 0
+int first_battlefield[COL_FIELD*ROWS_FIELD] = {
+  2, 2, 0, 3, 3, 3, 0, 0, 0, 0, //Row 0 //
   0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
   0, 2, 2, 0, 2, 2, 0, 0, 0, 3,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
@@ -28,7 +32,7 @@ int first_battlefield[10*10] = {
   0, 5, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 5, 0, 2, 2, 0, 3, 3, 3, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-};
+}; //controlsum is: 5, 1, 5, 1, 5, 1, 5, 1, 6, 0
 
 //overrides _write so we can use printf
 // The printf function is supported through the custom _write() function,
@@ -46,16 +50,41 @@ int _write(int handle, char *data, int size)
     return size;
 }
 
+void fieldController(int *x, int *y, int field[]){
+
+  for (int row = 0; row < x; row++) { // Loop through each row
+    for (int col = 0; col < y; col++) // Loop through each collumn
+      LOG("%d\t", field[row * COL_FIELD + col]);
+  }
+}
+
 void response(char question[])
 {
-  if(strncmp (question, "HD_STA", 6) ==0) //Start
-  {
+  if(strncmp (question, "HD_ST", 5) ==0){ //Start
     LOG("DH_START_%s\n", USER_NAME); // Respond with the user's name
   }
 
+  if(strncmp (question, "HD_BO", 5) ==0){ //Attack
+    LOG("DH_START_%s\n", USER_NAME); // Respond with the user's name
+  }
 
+  if(strncmp (question, "HD_CS", 5) ==0){ //Checksumm
+    LOG("DH_CS_");
+    int checksum [COL_FIELD] = {0}; // Initialize checksum array
+    for (int row = 0; row < ROWS_FIELD; row++){ // splitting the Rows from the Columns{
+      for (int col = 0; col < COL_FIELD; col++){ //summ for each Row
+        if (active_battlefield[row * COL_FIELD + col] != 0){
+          checksum[row]++;
+        }
+      }
+      LOG("%d", checksum[row]);
+    }
+    LOG("\n");
+  }
 
-
+  if(strncmp (question, "HD_SF", 5) ==0){ //Start
+    LOG("DH_START_%s\n", USER_NAME); // Respond with the user's name
+  }
 
 }
 
@@ -69,13 +98,10 @@ void USART2_IRQHandler(void)
     uint8_t c = USART2->RDR;                     // Read received byte from RDR (this automatically clears the RXNE flag)
     ret = fifo_put((Fifo_t *)&usart_rx_fifo, c); // Put incoming Data into the FIFO Buffer for later handling
   }
-
   USART2->ICR = 0xffffffff;
 }
 
-
-int main(void)
-{
+void config_hardware(void){
   SystemClock_Config(); // Configure the system clock to 48 MHz
 
   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;    // Enable GPIOA clock
@@ -99,33 +125,56 @@ int main(void)
   uint32_t uart_pri_encoding = NVIC_EncodePriority(0, 1, 0); // Encode priority: group 1, subpriority 0
   NVIC_SetPriority(USART2_IRQn, uart_pri_encoding);          // Set USART2 interrupt priority
   NVIC_EnableIRQ(USART2_IRQn);                               // Enable USART2 interrupt
+}
 
+int main(void)
+{
+
+  config_hardware(); // Function to configure the hardware
 
   fifo_init((Fifo_t *)&usart_rx_fifo);                       // Init the FIFO
 
   uint32_t bytes_recv = 0;
   char message[MESSAGE_SIZE]; // Buffer to store the received message
 
+  uint8_t saved_char = 0; // Counter for the number of characters saved in the question buffer
+  bool able_to_save = false; // Flag to indicate if the message is ready to be sent
+  char question[5] = {0}; // Null-terminate the string
+
+
   for (;;)
   { // Infinite loop
 
     
     uint8_t byte;
-    if (fifo_get((Fifo_t *)&usart_rx_fifo, &byte) == 0)
-    {
+    if (fifo_get((Fifo_t *)&usart_rx_fifo, &byte) == 0){
       if (bytes_recv < MESSAGE_SIZE){
-
         message[bytes_recv] = byte; // Store the received byte in the message buffer
-      
-        if(message[bytes_recv] == 'H') // Check for end of message
-        {
-          response(message); // Call the response function with the received message
 
-        }        
+                    //extract the question from the message
+        //sets bool variable to start saving the question
+        if(message[bytes_recv] == 'H'){
+          able_to_save = true;
+        }
+
+        //saves 5 character in the question buffer
+        if(able_to_save && saved_char < 5){
+          question[saved_char]= message[bytes_recv]; // Increment the saved character count] = message[bytes_recv];
+          saved_char++; // Increment the saved character count
+
+          //resets the question buffer, bool variable and saved_char counter
+        } else if (able_to_save && saved_char >= 5){
+          response(question); // Call the response function with the question
+          able_to_save = false; // Reset the flag after processing the question
+          memset(question, 0, sizeof(question)); // Reset the question buffer
+          saved_char = 0; // Reset the saved character count
+        }    
+
+
+
+        
       }
       bytes_recv++; // count the Bytes Received by getting Data from the FIFO
     }
   }
 }
-
-
