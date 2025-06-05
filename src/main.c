16ec1da104fe7b2baf879e7aea35cdda77fc5972
oubi-aed <alexander.oberhoer23@gmail.com>
2025-06-05 11:29:37 +0200

    UART initialised->can send data with log

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 
 #define MESSAGE_SIZE 64 // Size of the message buffer
 
+const char NAME[] = "Alexander"; // Name of the user, can be used for identification
+
 volatile Fifo_t usart_rx_fifo;
 
 const uint8_t USART2_RX_PIN = 3; // PA3 is used as USART2_RX
@@ -28,6 +30,22 @@ int first_battlefield[10*10] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
+//overrides _write so we can use printf
+int _write(int handle, char *data, int size)
+{
+    int count = size;
+    while (count--)
+    {
+        while (!(USART2->ISR & USART_ISR_TXE))
+        {
+        };
+        USART2->TDR = *data++;
+    }
+    return size;
+}
+
+
+
 void response (char*message, int start, int end)
 {
   char question [20];
@@ -36,7 +54,8 @@ void response (char*message, int start, int end)
   }
 
   if (strcmp(question, "HD_START") == 0){
-    asm("nop");
+
+    LOG("DH_%s\n", NAME);
   }
   else if (strcmp(question, "HD_BOOM") == 0){
     asm("nop");
@@ -58,6 +77,8 @@ void USART2_IRQHandler(void)
     uint8_t c = USART2->RDR;                     // Read received byte from RDR (this automatically clears the RXNE flag)
     ret = fifo_put((Fifo_t *)&usart_rx_fifo, c); // Put incoming Data into the FIFO Buffer for later handling
   }
+
+  USART2->ICR = 0xffffffff;
 }
 
 
@@ -68,21 +89,25 @@ int main(void)
   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;    // Enable GPIOA clock
   RCC->APB1ENR |= RCC_APB1ENR_USART2EN; // Enable USART2 clock
 
+  // Configure PA2 and PA3 for USART2 (TX and RX)
   GPIOA->MODER |= 0b10 << (USART2_TX_PIN * 2);    // Set PA2 to Alternate Function mode
   GPIOA->AFR[0] |= 0b0001 << (4 * USART2_TX_PIN); // Set AF for PA2 (USART2_TX)
   GPIOA->MODER |= 0b10 << (USART2_RX_PIN * 2);    // Set PA3 to Alternate Function mode
   GPIOA->AFR[0] |= 0b0001 << (4 * USART2_RX_PIN); // Set AF for PA3 (USART2_RX)
 
+  // Configure USART2
   USART2->BRR = (APB_FREQ / BAUDRATE); // Set baud rate (requires APB_FREQ to be defined)
   USART2->CR1 |= 0b1 << 2;             // Enable receiver (RE bit)
   USART2->CR1 |= 0b1 << 3;             // Enable transmitter (TE bit)
   USART2->CR1 |= 0b1 << 0;             // Enable USART (UE bit)
   USART2->CR1 |= 0b1 << 5;             // Enable RXNE interrupt (RXNEIE bit)
 
+  // Configure NVIC for USART2 interrupt
   NVIC_SetPriorityGrouping(0);                               // Use 4 bits for priority, 0 bits for subpriority
   uint32_t uart_pri_encoding = NVIC_EncodePriority(0, 1, 0); // Encode priority: group 1, subpriority 0
   NVIC_SetPriority(USART2_IRQn, uart_pri_encoding);          // Set USART2 interrupt priority
   NVIC_EnableIRQ(USART2_IRQn);                               // Enable USART2 interrupt
+
 
   fifo_init((Fifo_t *)&usart_rx_fifo);                       // Init the FIFO
 
@@ -92,6 +117,7 @@ int main(void)
   for (;;)
   { // Infinite loop
 
+    
     uint8_t byte;
     if (fifo_get((Fifo_t *)&usart_rx_fifo, &byte) == 0)
     {
@@ -101,6 +127,10 @@ int main(void)
 
         message[bytes_recv] = byte; // Store the received byte in the message buffer
         message_start++;
+        LOG("DH_%s\n", NAME);
+
+        
+
               // Log the received byte using printf via the LOG macro
       // The printf function is supported through the custom _write() function,
       // which redirects output to UART (USART2)
